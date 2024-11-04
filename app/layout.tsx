8bf1d6f1553e8c7e7b048f00ef878d738b6f5767
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-11-04 20:35:13 +0400

    feature/password change logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local';
 import './globals.css';
 import { auth } from '@/auth';
 import LogoutBtn from '@/components/logout-btn/LogoutBtn';
+import { Toaster } from '@/components/ui/toaster';
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
   variable: '--font-geist-sans',
@@ -43,6 +44,7 @@ export default async function RootLayout({
             'NO Currently logged user'
           )}
         </div> */}
+        <Toaster />
         {children}
       </body>
     </html>
